8c79850959efce13f3511cbf1cb77166c9c7cf2e
Lavinia Bertuzzi <lavinia.bertuzzi@overapp.it>
2023-03-07 23:52:55 +0100

    Mopdule rename & code cleaning

--- RNBridgeApp/ios/RNBridgeApp-Bridging-Header.h
+++ RNBridgeApp/ios/RNBridgeApp-Bridging-Header.h
@@ -2,10 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#ifndef RNBridgeApp-Bridging-Header
-#define RNBridgeApp-Bridging-Header
-
-
-#endif
 
 #import "React/RCTBridgeModule.h"
