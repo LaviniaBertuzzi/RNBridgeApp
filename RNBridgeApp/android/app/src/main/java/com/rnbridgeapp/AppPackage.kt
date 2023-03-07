165dfe8384bcf4bdf6e367a6de35004c6e3c5ecf
Mauro Vacchio <mauro.vacchio@overapp.it>
2023-03-07 12:51:32 +0100

    Implemented Kotlin module with method to show Toast

---
+++ RNBridgeApp/android/app/src/main/java/com/rnbridgeapp/AppPackage.kt
@@ -0,0 +1,21 @@
+package com.rnbridgeapp
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+import com.rnbridgeapp.ToastModule
+
+class AppPackage : ReactPackage {
+
+    override fun createViewManagers(
+        reactContext: ReactApplicationContext
+    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+
+    override fun createNativeModules(
+        reactContext: ReactApplicationContext
+    ): MutableList<NativeModule> = listOf(ToastModule(reactContext)).toMutableList()
+
+}
