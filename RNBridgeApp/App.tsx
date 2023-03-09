a0b3be86ec909c994b4b4933361c886b6f73249d
Lavinia Bertuzzi <lavinia.bertuzzi@overapp.it>
2023-03-09 15:45:28 +0100

    Code cleaning on App.tsx

--- RNBridgeApp/App.tsx
+++ RNBridgeApp/App.tsx
@@ -8,25 +8,6 @@
 import React from 'react';
 import { NativeModules, View, StyleSheet, TouchableOpacity, Text, Platform } from 'react-native';
 
-const RNBridgeApp = () => {
-
-  const {AlertModule} = NativeModules
-
-  const onPress = () => {
-    Platform.OS === 'android'
-    ? AlertModule.showAlert('Android Alert on stage!')
-    : AlertModule.showAlert('iOS Alert on stage')
-  };
-
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={onPress} >
-        <Text style={styles.text}>Click to invoke your native code</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -44,5 +25,25 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
 });
+
+const RNBridgeApp = () => {
+
+  const { AlertModule } = NativeModules
+
+  const onPress = () => {
+     // Message based on platform
+     const alertMessage =  Platform.OS === 'android' ? 'Android Alert on stage!' : 'iOS Alert on stage'
+     // Show the alert with the title
+     AlertModule.showAlert(alertMessage)
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={onPress} >
+        <Text style={styles.text}>Click to invoke your native code</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default RNBridgeApp;
